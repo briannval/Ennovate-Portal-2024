22fca71abe81ce13315a6abc3f166bc40332b0a1
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-06 12:15:42 +0700

    Initiialized team member model

---
+++ src/models/TeamMember.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface ITeamMember {
+  name: string;
+  email: string;
+  title: string;
+  image: string;
+}
+
+mongoose.Promise = global.Promise;
+
+const TeamMemberSchema: Schema = new Schema<ITeamMember>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const TeamMember =
+  (mongoose.models.TeamMember as mongoose.Model<ITeamMember>) ||
+  mongoose.model<ITeamMember>("TeamMember", TeamMemberSchema);
+
+export default TeamMember;
