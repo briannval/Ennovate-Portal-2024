41d2712b375e5c9c50ec1f3469afa052cc4059cf
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-07 16:42:21 +0700

    Added footer social constants

---
+++ src/constants/footer.ts
@@ -0,0 +1,17 @@
+export const footerSocialsLinks = [
+  {
+    img: "/email-logo.png",
+    href: "mailto:admin@ennovateubc.org",
+  },
+  {
+    img: "/ig-logo.png",
+    href: "https://www.instagram.com/ennovateubc/",
+  },
+  {
+    img: "/fb-logo.png",
+    href: "https://www.facebook.com/ennovateubc/",
+  },
+];
+
+export const CONTACT_GOOGLE_FORM_LINK =
+  "https://docs.google.com/forms/d/e/1FAIpQLSe9aX22hfqjL20yaWa-DIx-5Vc9SYelcrWh_kBEoQDLfRNRGw/viewform?usp=sf_link";
