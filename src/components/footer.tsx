f2ffb78c8ac23b4a00ffd1483508700e2a5a4d62
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-08 11:03:04 +0700

    Responsiveness in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,13 +8,13 @@ const Footer = () => {
   return (
     <footer className="bg-ennovate-main py-8 w-full">
       <div className="flex flex-col items-center">
-        <h2 className="font-bold text-2xl mb-6 text-center text-white">
+        <h2 className="font-bold text-xl md:text-2xl mb-6 text-center text-white">
           Interested in bringing Ennovate to your class?
         </h2>
-        <button className="text-ennovate-dark-blue text-2xl font-extrabold bg-ennovate-yellow hover:bg-white rounded-3xl py-2 px-6 text-center mb-12">
+        <button className="text-ennovate-dark-blue text-xl md:text-2xl font-extrabold bg-ennovate-yellow hover:bg-white rounded-3xl py-2 px-6 text-center mb-12">
           CONTACT US
         </button>
-        <h2 className="font-extralight text-2xl mb-6 text-center text-white">
+        <h2 className="font-extralight text-xl md:text-2xl mb-6 text-center text-white">
           Made with love from the Ennovate Team ♥
         </h2>
         <div className="flex justify-between w-full max-w-screen-xl px-4">
@@ -23,7 +23,7 @@ const Footer = () => {
               <Link
                 key={index}
                 href={footerSocialsLink.href}
-                className="max-w-[30px] m-[5px]"
+                className="max-w-[20px] md:max-w-[30px] m-[5px]"
               >
                 <Image
                   src={footerSocialsLink.img}
