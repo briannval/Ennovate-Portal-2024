a19559ea5bf4b63a26794166c7f9d4ca6cd585ae
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-13 16:12:49 +0700

    Minor outsourcing

--- src/components/affiliations.tsx
+++ src/components/affiliations.tsx
@@ -1,11 +1,6 @@
+import { AFFILIATION_IMAGES } from "@/constants/affiliations";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
-
-const AFFILIATION_IMAGES = [
-  { src: "/ams-logo.png", name: "AMS" },
-  { src: "/enactus-logo.png", name: "Enactus" },
-  { src: "/ubc-logo.png", name: "UBC" },
-];
 
 const Affiliations = () => {
   return (
---
+++ src/constants/affiliations.ts
@@ -0,0 +1,5 @@
+export const AFFILIATION_IMAGES = [
+  { src: "/ams-logo.png", name: "AMS" },
+  { src: "/enactus-logo.png", name: "Enactus" },
+  { src: "/ubc-logo.png", name: "UBC" },
+];
