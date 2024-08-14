5f56909c64becebca0c0d74b956ffaab4ae20ae7
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-14 16:02:12 +0700

    Add auth to firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -12,5 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
 
 export const storage = getStorage(app);
