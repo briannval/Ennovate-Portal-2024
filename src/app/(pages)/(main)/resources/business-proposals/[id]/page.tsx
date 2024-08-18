403855b9f7047aa41075e24b858eda34abb41e96
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-18 22:37:36 +0700

    Fixed loading state for business proposal dynamic

--- src/app/(pages)/(main)/resources/business-proposals/[id]/page.tsx
+++ src/app/(pages)/(main)/resources/business-proposals/[id]/page.tsx
@@ -22,23 +22,25 @@ export default function BusinessProposals({
     setData();
   }, [params.id]);
 
-  if (!businessProposal) {
-    return <Loading />;
-  }
-
   return (
     <PageCenteringWrapper>
-      <h1 className="text-4xl text-center font-extrabold tracking-tight text-ennovate-dark-blue sm:text-5xl mt-4">
-        {businessProposal.name}
-      </h1>
-      <p className="mt-2 text-xl text-center font-bold leading-8 text-ennovate-dark-blue opacity-70 mb-8">
-        {businessProposal.description}
-      </p>
-      <embed
-        src={businessProposal.drive}
-        type="application/pdf"
-        className="w-screen max-w-screen-md h-[600px] rounded-lg mx-8"
-      />
+      {businessProposal ? (
+        <>
+          <h1 className="text-4xl text-center font-extrabold tracking-tight text-ennovate-dark-blue sm:text-5xl mt-4">
+            {businessProposal.name}
+          </h1>
+          <p className="mt-2 text-xl text-center font-bold leading-8 text-ennovate-dark-blue opacity-70 mb-8">
+            {businessProposal.description}
+          </p>
+          <embed
+            src={businessProposal.drive}
+            type="application/pdf"
+            className="w-screen max-w-screen-md h-[600px] rounded-lg mx-8"
+          />
+        </>
+      ) : (
+        <Loading />
+      )}
     </PageCenteringWrapper>
   );
 }
