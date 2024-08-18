40db92cb477d44303574785f8ec2493f3316deea
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-19 00:55:37 +0700

    Minor

--- src/components/businessProposal.tsx
+++ src/components/businessProposal.tsx
@@ -1,5 +1,6 @@
 import { IBusinessProposal } from "@/models/BusinessProposal";
 import Image from "next/image";
+import Link from "next/link";
 
 const BusinessProposal = ({
   businessProposal,
@@ -7,25 +8,27 @@ const BusinessProposal = ({
   businessProposal: IBusinessProposal;
 }) => {
   return (
-    <div className="max-w-sm bg-white border border-ennovate-main rounded-lg hover:shadow hover:shadow-ennovate-main">
-      <div className="relative w-full h-64">
-        <Image
-          className="rounded-t-lg"
-          src={businessProposal.image}
-          alt="Noteworthy technology acquisitions 2021"
-          layout="fill"
-          objectFit="cover"
-        />
+    <Link href={`/resources/business-proposals/${businessProposal._id}`}>
+      <div className="max-w-sm bg-white border border-ennovate-main rounded-lg hover:shadow hover:shadow-ennovate-main">
+        <div className="relative w-full h-64">
+          <Image
+            className="rounded-t-lg"
+            src={businessProposal.image}
+            alt="Noteworthy technology acquisitions 2021"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className="p-5">
+          <h5 className="text-2xl font-bold tracking-tight text-ennovate-dark-blue">
+            {businessProposal.name}
+          </h5>
+          <h5 className="mb-2 text-xl font-medium tracking-tight text-ennovate-dark-blue opacity-80">
+            {businessProposal.description}
+          </h5>
+        </div>
       </div>
-      <div className="p-5">
-        <h5 className="text-2xl font-bold tracking-tight text-ennovate-dark-blue">
-          {businessProposal.name}
-        </h5>
-        <h5 className="mb-2 text-xl font-medium tracking-tight text-ennovate-dark-blue opacity-80">
-          {businessProposal.description}
-        </h5>
-      </div>
-    </div>
+    </Link>
   );
 };
 
