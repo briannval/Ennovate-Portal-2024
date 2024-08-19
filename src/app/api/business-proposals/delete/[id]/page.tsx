2527255f66d1c2c996a9a8426fa2c73d6034e2b9
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-19 10:04:35 +0700

    Initalized delete route for business proposal

---
+++ src/app/api/business-proposals/delete/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { connectToDatabase } from "@/lib/mongoose";
+import { redis } from "@/lib/redis";
+import BusinessProposal from "@/models/BusinessProposal";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(
+  _: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connectToDatabase();
+
+    const { id } = params;
+
+    await Promise.all([
+      BusinessProposal.findByIdAndDelete(id),
+      redis.del(`businessProposal:${id}`, "businessProposal"),
+    ]);
+
+    return NextResponse.json("Success");
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Failed to delete business proposal" },
+      { status: 500 }
+    );
+  }
+}
