eaaa068e2d9c7b98d6a8ff7ded72bfa13d4fede9
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-23 12:48:26 +0700

    Reorganized about photos

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -1,5 +1,5 @@
 export const ABOUT_ENNOVATE_IMAGES = [
-  "/about-ennovate-1.jpeg",
-  "/about-ennovate-2.jpeg",
-  "/about-ennovate-3.jpeg",
+  "/about/about-ennovate-1.jpeg",
+  "/about/about-ennovate-2.jpeg",
+  "/about/about-ennovate-3.jpeg",
 ];
