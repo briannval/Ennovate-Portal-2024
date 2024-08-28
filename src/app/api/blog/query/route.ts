eafa6d1f39cf1e7201ba8285dc93906285f87b0c
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-28 11:31:54 +0700

    Included link to actual blog

--- src/app/api/blog/query/route.ts
+++ src/app/api/blog/query/route.ts
@@ -66,6 +66,7 @@ export async function POST(req: NextRequest) {
           subtitle,
           readingTime,
           dateUploaded,
+          mediumUrl: blog.mediumUrl,
           featured: blog.featured,
         };
       })
--- src/components/blogSection.tsx
+++ src/components/blogSection.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import PageCenteringWrapper from "@/wrappers/pageCenteringWrapper";
 import Loading from "./loading";
+import Link from "next/link";
 
 interface BlogSectionType {
   coverImage: string;
@@ -11,6 +12,7 @@ interface BlogSectionType {
   subtitle: string;
   readingTime: string;
   dateUploaded: string;
+  mediumUrl: string;
   featured: boolean;
 }
 
@@ -45,30 +47,34 @@ export default function BlogSection({ featured }: { featured: boolean }) {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center gap-y-8 lg:gap-y-0 flex-wrap md:flex-wrap lg:flex-nowrap lg:flex-row lg:justify-between lg:gap-x-8">
           {blogs.map((blog, index) => (
-            <div
-              key={index}
-              className="group w-full max-lg:max-w-xl lg:w-1/3 border border-gray-300 rounded-2xl"
-            >
-              <div className="flex items-center">
-                <img
-                  src={blog.coverImage}
-                  alt={blog.title}
-                  className="rounded-t-2xl w-full"
-                />
+            <Link href={blog.mediumUrl} target="_blank">
+              <div
+                key={index}
+                className="group w-full max-lg:max-w-xl lg:w-1/3 border border-gray-300 rounded-2xl"
+              >
+                <div className="flex items-center">
+                  <img
+                    src={blog.coverImage}
+                    alt={blog.title}
+                    className="rounded-t-2xl w-full"
+                  />
+                </div>
+                <div className="p-4 lg:p-6 transition-all duration-300 rounded-b-2xl group-hover:bg-gray-50">
+                  <span className="text-ennovate-main font-medium mb-3 block">
+                    {blog.dateUploaded}
+                  </span>
+                  <h4 className="text-xl text-gray-900 font-medium leading-8 mb-2">
+                    {blog.title}
+                  </h4>
+                  <p className="text-gray-500 leading-6 mb-4">
+                    {blog.subtitle}
+                  </p>
+                  <p className="text-lg text-ennovate-main font-semibold">
+                    {blog.readingTime}
+                  </p>
+                </div>
               </div>
-              <div className="p-4 lg:p-6 transition-all duration-300 rounded-b-2xl group-hover:bg-gray-50">
-                <span className="text-ennovate-main font-medium mb-3 block">
-                  {blog.dateUploaded}
-                </span>
-                <h4 className="text-xl text-gray-900 font-medium leading-8 mb-2">
-                  {blog.title}
-                </h4>
-                <p className="text-gray-500 leading-6 mb-4">{blog.subtitle}</p>
-                <p className="text-lg text-ennovate-main font-semibold">
-                  Read more..
-                </p>
-              </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
