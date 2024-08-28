256fce5bef50f1d01c19d72a365362657bc93a26
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-08-29 06:33:09 +0700

    Minor

--- cypress/component/about.cy.tsx
+++
@@ -1,9 +0,0 @@
-import About from "@/components/about";
-
-describe("<About />", () => {
-  it("should render and display expected content", () => {
-    cy.mount(<About />);
-
-    cy.get("h1").contains("About Us");
-  });
-});
