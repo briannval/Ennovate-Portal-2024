47c4313d7fe6a802af132911bfc02d2b4d0c09a5
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-09-01 19:57:33 -0700

    Hotfix 2: Enactus to Enactus UBC on affiliations

--- src/constants/affiliations.ts
+++ src/constants/affiliations.ts
@@ -2,7 +2,7 @@ export const AFFILIATION_IMAGES = [
   { src: "/logos/ams-logo.webp", name: "AMS", href: "https://www.ams.ubc.ca/" },
   {
     src: "/logos/enactus-logo.webp",
-    name: "Enactus",
+    name: "Enactus UBC",
     href: "https://enactusubc.ca/",
   },
   { src: "/logos/ubc-logo.webp", name: "UBC", href: "https://www.ubc.ca/" },
