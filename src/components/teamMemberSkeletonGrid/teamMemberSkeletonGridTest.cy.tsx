8e64cf0d772f9cc9bb05d21d8dcaecbca297eda5
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-09-05 17:33:23 -0700

    Responsiveness for skeleton grid team members

--- src/components/teamMemberSkeletonGrid/teamMemberSkeletonGridTest.cy.tsx
+++ src/components/teamMemberSkeletonGrid/teamMemberSkeletonGridTest.cy.tsx
@@ -10,3 +10,26 @@ describe("<TeamMemberSkeletonGrid />", () => {
       .should("have.length", 12);
   });
 });
+
+describe("Responsiveness", () => {
+  it("1 col base breakpoint", () => {
+    cy.viewport("iphone-se2");
+    cy.mount(<TeamMemberSkeletonGrid />);
+    cy.checkGridColumns("team-member-skeleton-grid", 1);
+  });
+  it("2 col md breakpoint", () => {
+    cy.viewport("ipad-mini");
+    cy.mount(<TeamMemberSkeletonGrid />);
+    cy.checkGridColumns("team-member-skeleton-grid", 2);
+  });
+  it("3 col lg breakpoint", () => {
+    cy.viewport(1024, 800);
+    cy.mount(<TeamMemberSkeletonGrid />);
+    cy.checkGridColumns("team-member-skeleton-grid", 3);
+  });
+  it("4 col xl breakpoint", () => {
+    cy.viewport("macbook-16");
+    cy.mount(<TeamMemberSkeletonGrid />);
+    cy.checkGridColumns("team-member-skeleton-grid", 4);
+  });
+});
