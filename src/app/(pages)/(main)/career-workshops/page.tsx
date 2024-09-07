a43197b5cf28b4f76f6c5a51ffb59f5a2f86543c
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-09-06 20:00:09 -0700

    Styled page title

--- src/app/(pages)/(main)/career-workshops/page.tsx
+++ src/app/(pages)/(main)/career-workshops/page.tsx
@@ -1,3 +1,11 @@
+import PageCenteringWrapper from "@/wrappers/pageCenteringWrapper";
+
 export default function CareerWorkshops() {
-  return <h1>Career Workshops</h1>;
+  return (
+    <PageCenteringWrapper>
+      <h1 className="text-center font-extrabold text-5xl mt-8 text-ennovate-dark-blue mx-8">
+        Career Workshops
+      </h1>
+    </PageCenteringWrapper>
+  );
 }
