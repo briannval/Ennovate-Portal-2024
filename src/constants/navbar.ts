bb9ae5c456e06ef8e8b5a4ecab23c96b93b074bc
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-09-06 19:55:25 -0700

    Add link to navbar

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -2,5 +2,5 @@ export const navbarLinks = [
   { name: "THE TEAM", href: "/team" },
   { name: "FINAL COMP", href: "/comp" },
   { name: "RESOURCES", href: "/resources" },
-  { name: "CAREER WORKSHOPS", href: "/" },
+  { name: "CAREER WORKSHOPS", href: "/career-workshops" },
 ];
