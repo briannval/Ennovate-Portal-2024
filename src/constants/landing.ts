8da861fad7b7de8f4ed39cc959829fb675f37a97
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-09-20 16:00:08 -0700

    Update landing one-liners

---
+++ src/constants/landing.ts
@@ -0,0 +1,6 @@
+export const TYPEWRITER_ONE_LINERS = [
+  "Launching mini-social enterprises",
+  "Hands-on entrepreneurial experience for youth",
+  "Dynamic pitch competition for high school students",
+  "4-month intensive entrepreneurship curriculum",
+];
