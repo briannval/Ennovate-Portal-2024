16e032a95828a660dc12f8cca0c06aa1c670b174
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-11-12 23:39:33 -0800

    Update component test

--- src/components/teamMemberGrid/teamMemberGridTest.cy.tsx
+++ src/components/teamMemberGrid/teamMemberGridTest.cy.tsx
@@ -4,9 +4,9 @@ import { MockAuthProvider } from "../../../cypress/mocks/auth/auth";
 import { ITeamMember } from "@/models/TeamMember";
 
 describe("<TeamMemberGrid /> authenticated", () => {
-  it("cta should exist outside grid", () => {
+  it("cta should exist outside grid last page", () => {
     cy.viewport("macbook-16");
-    cy.fixture("components/teamMemberGrid").then((teamMemberGrid) => {
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
       cy.mount(
         <MockAuthProvider isAuthenticated={true}>
           <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage />
@@ -19,7 +19,22 @@ describe("<TeamMemberGrid /> authenticated", () => {
     }); // exists outside grid
   });
 
-  it("cta should exist inside grid", () => {
+  it("cta should not exist not last page", () => {
+    cy.viewport("macbook-16");
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
+      cy.mount(
+        <MockAuthProvider isAuthenticated={true}>
+          <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage={false} />
+        </MockAuthProvider>
+      );
+    });
+    cy.get('[data-cy="add-team-member-cta"]').should("not.exist");
+    cy.get('[data-cy="team-member-grid"]').within(() => {
+      cy.get('[data-cy="add-team-member-cta"]').should("not.exist");
+    }); // exists outside grid
+  });
+
+  it("cta should exist inside grid last page", () => {
     cy.viewport("macbook-16");
     cy.fixture("components/teamMemberGrid").then(
       (teamMemberGrid: ITeamMember[]) => {
@@ -38,12 +53,36 @@ describe("<TeamMemberGrid /> authenticated", () => {
 });
 
 describe("<TeamMemberGrid /> unauthenticated", () => {
-  it("cta should not exist", () => {
+  it("cta should not exist last page 11 members", () => {
     cy.viewport("macbook-16");
-    cy.fixture("components/teamMemberGrid").then((teamMemberGrid) => {
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
+      cy.mount(
+        <MockAuthProvider isAuthenticated={false}>
+          <TeamMemberGrid teamMembers={teamMemberGrid.slice(0, -1)} isLastPage />
+        </MockAuthProvider>
+      );
+    });
+    cy.get('[data-cy="add-team-member-cta"]').should("not.exist");
+  });
+
+  it("cta should not exist last page 12 members", () => {
+    cy.viewport("macbook-16");
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
       cy.mount(
         <MockAuthProvider isAuthenticated={false}>
           <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage />
+        </MockAuthProvider>
+      );
+    });
+    cy.get('[data-cy="add-team-member-cta"]').should("not.exist");
+  });
+
+  it("cta not exist not last page", () => {
+    cy.viewport("macbook-16");
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
+      cy.mount(
+        <MockAuthProvider isAuthenticated={false}>
+          <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage={false} />
         </MockAuthProvider>
       );
     });
@@ -54,7 +93,7 @@ describe("<TeamMemberGrid /> unauthenticated", () => {
 describe("Responsiveness", () => {
   it("1 col base breakpoint", () => {
     cy.viewport("iphone-se2");
-    cy.fixture("components/teamMemberGrid").then((teamMemberGrid) => {
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
       cy.mount(
         <MockAuthProvider isAuthenticated={false}>
           <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage />
@@ -66,7 +105,7 @@ describe("Responsiveness", () => {
   });
   it("2 col md breakpoint", () => {
     cy.viewport("ipad-mini");
-    cy.fixture("components/teamMemberGrid").then((teamMemberGrid) => {
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
       cy.mount(
         <MockAuthProvider isAuthenticated={false}>
           <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage />
@@ -78,7 +117,7 @@ describe("Responsiveness", () => {
   });
   it("3 col lg breakpoint", () => {
     cy.viewport(1024, 800);
-    cy.fixture("components/teamMemberGrid").then((teamMemberGrid) => {
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
       cy.mount(
         <MockAuthProvider isAuthenticated={false}>
           <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage />
@@ -90,7 +129,7 @@ describe("Responsiveness", () => {
   });
   it("4 col xl breakpoint", () => {
     cy.viewport("macbook-16");
-    cy.fixture("components/teamMemberGrid").then((teamMemberGrid) => {
+    cy.fixture("components/teamMemberGrid").then((teamMemberGrid: ITeamMember[]) => {
       cy.mount(
         <MockAuthProvider isAuthenticated={false}>
           <TeamMemberGrid teamMembers={teamMemberGrid} isLastPage />
