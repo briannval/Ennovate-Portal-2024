0dab01a4e1f40161a59e77ebf5ebefaeb7adebca
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-11-14 16:55:34 -0800

    Mock User

--- cypress/mocks/auth/auth.tsx
+++ cypress/mocks/auth/auth.tsx
@@ -1,9 +1,43 @@
 import { AuthContext } from "@/contexts/AuthContext";
+import { IdTokenResult, User } from "firebase/auth";
 
 interface MockAuthProviderProps {
   children: React.ReactNode;
   isAuthenticated?: boolean;
 }
+
+const mockUser: User = {
+  uid: 'mock-uid',
+  email: 'mock-user@example.com',
+  displayName: 'Mock User',
+  photoURL: 'http://example.com/photo.jpg',
+  emailVerified: true,
+  isAnonymous: false,
+  metadata: {
+    creationTime: undefined,
+  },
+  providerData: [],
+  refreshToken: "",
+  tenantId: null,
+  delete: function (): Promise<void> {
+    throw new Error("Function not implemented.");
+  },
+  getIdToken: function (forceRefresh?: boolean | undefined): Promise<string> {
+    throw new Error("Function not implemented.");
+  },
+  getIdTokenResult: function (forceRefresh?: boolean | undefined): Promise<IdTokenResult> {
+    throw new Error("Function not implemented.");
+  },
+  reload: function (): Promise<void> {
+    throw new Error("Function not implemented.");
+  },
+  toJSON: function (): object {
+    throw new Error("Function not implemented.");
+  },
+  phoneNumber: null,
+  providerId: ""
+};
+
 
 export const MockAuthProvider = ({
   children,
@@ -14,6 +48,7 @@ export const MockAuthProvider = ({
     isLoading: false,
     login: cy.stub(),
     logout: cy.stub(),
+    user: mockUser,
   };
 
   return (
