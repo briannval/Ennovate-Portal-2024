7c7d8536880b35fc3bfe26c1afdfc1e6a66bd533
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-11-24 21:11:44 -0800

    Update API route

--- src/app/api/projects/create/route.ts
+++ src/app/api/projects/create/route.ts
@@ -8,9 +8,9 @@ export async function POST(request: NextRequest) {
   try {
     await connectToDatabase();
 
-    const { name, description, businessProposal, blog } = await request.json();
+    const { name, description, presentation_slides, businessProposal, blog } = await request.json();
 
-    await Project.create({ name, description, businessProposal, blog });
+    await Project.create({ name, description, presentation_slides, businessProposal, blog });
 
     await redis.del(["project"]);
 
