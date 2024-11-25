789c14a86d2568dcee2aae6aec323e93534af7e4
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-11-24 21:22:27 -0800

    Added specific query project route

---
+++ src/app/api/projects/query/[id]/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/mongoose";
+import { redis } from "@/lib/redis";
+import { CACHE_KEY_EXPIRY_TIME } from "@/constants/actions";
+import { captureException } from "@sentry/nextjs";
+import Project from "@/models/Project";
+
+export async function GET(_: NextRequest,
+  { params }: { params: { id: string } }
+    ) {
+  try {
+    await connectToDatabase();
+
+    const { id } = params;
+
+    const project = await Project.findById(id);
+
+    return NextResponse.json(project);
+  } catch (e) {
+    captureException(e);
+    return NextResponse.json(
+      { message: "Failed to fetch projects" },
+      { status: 500 }
+    );
+  }
+}
