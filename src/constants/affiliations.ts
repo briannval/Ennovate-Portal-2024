bef9d8b5ff587729a29816c73c9e1ba70348bd88
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-04-05 13:04:32 -0700

    Add Fjallraven

--- src/constants/affiliations.ts
+++ src/constants/affiliations.ts
@@ -6,8 +6,9 @@ export const AFFILIATION_IMAGES = [
     href: "https://enactusubc.ca/",
   },
   { src: "/logos/ubc-logo.webp", name: "UBC", href: "https://www.ubc.ca/" },
+  { src: "/logos/fjallraven-logo.webp", name: "Fjallraven", href: "https://www.fjallraven.com/"},
   { src: "/logos/teadot-logo.webp", name: "Teadot", href: "https://www.teadotdrinks.com/"},
   { src: "/logos/jukebox-logo.webp", name: "JukeBox", href: "https://www.jukeboxprint.com/"},
   { src: "/logos/wizeprep-logo.webp", name: "Wizeprep", href: "https://www.wizeprep.com/"},
-  { src: "/logos/steves-poke-logo.webp", name: "Steve's Poké Bar", href: "https://www.stevespokebar.ca/"}
+  { src: "/logos/steves-poke-logo.webp", name: "Steve's Poké Bar", href: "https://www.stevespokebar.ca/"},
 ];
